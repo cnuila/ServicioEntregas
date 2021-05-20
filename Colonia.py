d1144538827c021854b094f57866db17d6b74c53
Daniel Agurcia <danielagurcia90@gmail.com>
2021-05-20 11:37:13 -0600

    cargar colonias agreagado

--- Colonia.py
+++ Colonia.py
@@ -1,5 +1,8 @@
-class Colonia:
+import json
 
+
+class Colonia:
+    import json
     __nombre = ""
     __rutas = {}
 
@@ -9,10 +12,22 @@ class Colonia:
 
     def getNombre(self):
         return self.__nombre
-    
+
     def getRutas(self):
         return self.__rutas
 
     @staticmethod
     def cargarColonias(file):
-        pass
+        # read file
+        myJsonFile = open(file, 'r')
+        JsonData = myJsonFile.read()
+        # parse
+        obj = json.loads(JsonData)
+        listaColonias = obj['colonias']
+        cantidadColonias = len(listaColonias)
+        print(cantidadColonias)
+        for i in range(len(listaColonias)):
+            print("Colonia: ", listaColonias[i].get("nombre"))
+        listaAdyacentes = listaColonias[i]["adyacentes"]
+        for j in range(len(listaAdyacentes)):
+            print("Colonia Adyacente: ", listaAdyacentes[j].get("nombre"), " Distancia: ", listaAdyacentes[j].get("distancia"), "\n")
