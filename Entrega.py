ee50c60e717ba2d0132b4c4eef07c2660f654abf
Daniel Agurcia <danielagurcia90@gmail.com>
2021-05-21 17:28:19 -0600

    pull

--- Entrega.py
+++ Entrega.py
@@ -4,6 +4,9 @@ from Ciudad import Ciudad
 from NodoBusqueda import NodoBusqueda
 from Estado import Estado
 import json
+from queue import Queue
+
+
 class Entrega:
 
     def __init__(self):
@@ -12,7 +15,7 @@ class Entrega:
         self.__lugaresEntregas = []
         self.__costoMaximo = 0
 
-    def cargarMapa(nombreArchivo):
+    def cargarMapa(self, nombreArchivo):
         # read file
         myJsonFile = open(nombreArchivo, 'r')
         JsonData = myJsonFile.read()
@@ -20,25 +23,25 @@ class Entrega:
         obj = json.loads(JsonData)
         listaColonias = obj['colonias']
 
-        #se crea la ciudad
+        # se crea la ciudad
         city = Ciudad(obj["ciudad"])
-        
+
         for i in range(len(listaColonias)):
             city.getColonias().append(Colonia.cargarColonias(listaColonias[i]))
-            
+        self.__ciudad = city
 
-    #función que retorna el nodo inicial de la busqueda
+    # función que retorna el nodo inicial de la busqueda
     def nodoInicial(self):
         coloniaInicial = self.__ciudad.getColonia(self.__coloniaInicial)
-        return NodoBusqueda(Estado(coloniaInicial, self.__lugaresEntregas),None,self.__coloniaInicial,0)
+        return NodoBusqueda(Estado(coloniaInicial, self.__lugaresEntregas), None, self.__coloniaInicial, 0)
 
     def hacerNodoHijo(self, padre, ruta, costoRuta):
         nuevaColonia = self.__ciudad.getColonia(ruta)
         coloniasFaltantes = padre.getEstado().nuevasColoniasFaltantes(ruta)
         nuevoCosto = padre.getCostoCamino() + costoRuta
         nuevaAccion = padre.getEstado().getNombreColoniaEstado() + "->" + ruta
-        return NodoBusqueda(Estado(nuevaColonia,coloniasFaltantes),padre,nuevaAccion,nuevoCosto)
-    
+        return NodoBusqueda(Estado(nuevaColonia, coloniasFaltantes), padre, nuevaAccion, nuevoCosto)
+
     def seEncuentraExplorados(self, explorados, estadoBuscar):
         for estado in explorados:
             if estado == estadoBuscar:
@@ -52,21 +55,22 @@ class Entrega:
         explorados = set()
         while True:
             if frontera.estaVacia():
-                return False #hacer algo failure
+                return False  # hacer algo failure
             nodo = frontera.pop()
             if nodo.getEstado().esEstadoMeta(self.__coloniaInicial):
-                return True #hacer algo solution
+                return True  # hacer algo solution
             explorados.add(nodo.getEstado())
             coloniaActual = nodo.getEstado().getColoniaEstado()
-            #las rutas adyacentes son las acciones validas por lo tanto se itera através de ellas
-            for ruta in coloniaActual.getRutas().keys():                
-                nodoHijo = self.hacerNodoHijo(nodo,ruta,coloniaActual.getRutas()[ruta])
+            # las rutas adyacentes son las acciones validas por lo tanto se itera através de ellas
+            for ruta in coloniaActual.getRutas().keys():
+                nodoHijo = self.hacerNodoHijo(
+                    nodo, ruta, coloniaActual.getRutas()[ruta])
                 seEncuentraFrontera = frontera.seEncuentra(nodoHijo)
                 if (not self.seEncuentraExplorados(explorados, nodoHijo.getEstado())) or (seEncuentraFrontera == -1):
                     frontera.insertar(nodoHijo)
-                elif seEncuentraFrontera != -1:                     
-                    frontera.intercambiarMejorEstado(nodoHijo,seEncuentraFrontera)
-    
+                elif seEncuentraFrontera != -1:
+                    frontera.intercambiarMejorEstado(
+                        nodoHijo, seEncuentraFrontera)
+
     def BFS(self):
         pass
-
